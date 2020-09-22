2bd4c4d015bd9deb1a4a2c42be5021aa87c5ccdb
Jason <jason.auth@gmail.com>
2020-09-21 20:05:42 -0400

    branch test

--- TestData/excelDemo.py
+++ TestData/excelDemo.py
@@ -20,14 +20,14 @@ import openpyxl
 #         for j in range(2, sheet.max_column+1):  # to get columns
 #             print(sheet.cell(row=i, column=j).value)
 #
+# #
+# datadict = {}
+# book = openpyxl.load_workbook("C:\\Users\\JD\\Documents\\PythonDemo.xlsx")  # Load excel sheet
+# sheet = book.active  # set sheet to active sheet
 #
-datadict = {}
-book = openpyxl.load_workbook("C:\\Users\\JD\\Documents\\PythonDemo.xlsx")  # Load excel sheet
-sheet = book.active  # set sheet to active sheet
-
-for i in range(1, sheet.max_row+1):  # to get rows
-    if sheet.cell(row=i, column=1) == "Testcase2":
-        for j in range(2, sheet.max_column+1):  # to get columns
-            # datadict["column header"] = cell value
-            datadict[sheet.cell(row=1, column=j).value] = sheet.cell(row=i, column=j).value
-            print([datadict])
+# for i in range(1, sheet.max_row+1):  # to get rows
+#     if sheet.cell(row=i, column=1) == "Testcase2":
+#         for j in range(2, sheet.max_column+1):  # to get columns
+#             # datadict["column header"] = cell value
+#             datadict[sheet.cell(row=1, column=j).value] = sheet.cell(row=i, column=j).value
+#             print([datadict])
